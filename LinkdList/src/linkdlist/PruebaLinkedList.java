1dfc9bac1a911f638d91eb68f4a79884c7cf70cd
Andres Chavez <andressalcido07@gmail.com>
2026-08-27 12:54:52 -0700

    Added index in linkdlist

--- LinkdList/src/linkdlist/PruebaLinkedList.java
+++ LinkdList/src/linkdlist/PruebaLinkedList.java
@@ -17,15 +17,18 @@ class Student {
 
 class LinkdList {
     Student head;
+    int index=0;
     public LinkdList(){
         this.head = null;
     }
     
+    //To add an student to the list
     public void add(String name, int score) {
         Student newStudent = new Student(name, score);
         if (head == null) {
             head = newStudent;
-            return;
+            this.index++;
+            return;            
         }
         Student current = head;
         while (current.next != null) {
@@ -33,6 +36,8 @@ class LinkdList {
         }
         current.next = newStudent;
     }
+    
+    //prints the list
     public void print(){
         Student current= this.head;
         while (current !=null) {
@@ -41,6 +46,7 @@ class LinkdList {
         } 
     }
     
+    //It is used for searching names
     public Student retrieve (String searchedName){
         Student current=this.head;
         while (current != null) {
@@ -52,23 +58,27 @@ class LinkdList {
         return null; //not found
     }
     
+    //Remove the student
     public void remove (int n){
         if (head == null || n<0) {return;} //Empty List; index<0; 
-        int index=0;
+        if (n>=this.index){return;}
         Student current=head;
         
         while (current != null && index < (n-1)) {            
             current = current.next;
-            index++;
+            
         }
         if (current==null || current.next ==null) {
             return; //Position out fo range
         }
         
         current.next= current.next.next;
+        index--;
         
-    }
-}
+    }//Close Remove
+    
+}//Close class LinkdList
+
 public class PruebaLinkedList {
 
     public static void main(String[] args) {
@@ -89,8 +99,5 @@ public class PruebaLinkedList {
         
         lista.remove(2);
         lista.print();
-    }
-    
-   
-    
-}
+    }  //Close Main
+}//Close class PruebaLinkedList
